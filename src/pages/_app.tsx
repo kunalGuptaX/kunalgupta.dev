9781ec5b2a149e6e9ce742a757f3f5fb71592f07
kunalguptax <kunal.gupta@oscape.in>
2022-01-11 00:00:06 +0530

    fixed styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,14 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "../styles/default.theme";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ThemeProvider theme={defaultTheme}>
+      <Head>
+        <title>Kunal Gupta</title>
+      </Head>
       <Component {...pageProps} />
     </ThemeProvider>
   );
