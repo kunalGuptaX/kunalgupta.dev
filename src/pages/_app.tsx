757fefcf3d3cff9a14d1879c485c439eb746787e
kunalguptax <kunal.gupta@oscape.in>
2022-01-11 00:42:00 +0530

    fix font

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/no-css-tags */
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ThemeProvider } from "styled-components";
@@ -9,6 +10,7 @@ function MyApp({ Component, pageProps }: AppProps) {
     <ThemeProvider theme={{}}>
       <Head>
         <title>Kunal Gupta</title>
+        <link href="/fonts/GTWalsheim/stylesheet.css" rel="stylesheet" />
       </Head>
       <Component {...pageProps} />
     </ThemeProvider>
